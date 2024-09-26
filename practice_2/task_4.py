103ab5a5a197b4d5fc1d78e9f2376dbc28b48618
Dmitry <dimasharonov95@yandex.ru>
2024-09-26 18:51:39 +0300

    Done

--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -4,3 +4,17 @@ Develop a program that asks the user for an integer 4-digit number and calculate
 3 + 1 + 4 + 1 = 9
 
 """
+
+
+def read_int(hint: str) -> int:
+    while True:
+        try:
+            return int(input(hint))
+        except ValueError:
+            print("Please enter a valid integer")
+
+
+n = 4
+integers = [read_int(f"Enter int {i}/{n}: ") for i in range(1, n + 1)]
+
+print(" + ".join(str(number) for number in integers), "=", sum(integers))
